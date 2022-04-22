a453747d89595054b499f230fb0962b292a7c0f1
hardik <hp676913@gmail.com>
2022-04-22 22:18:41 +0530

    Created Right Time Showing Clock With Valid JS

--- App.js
+++ App.js
@@ -3,11 +3,7 @@ setInterval(() => {
     let min = document.getElementById('min');
     let sec = document.getElementById('sec');
     let hr = document.getElementById('hr');
-    hr.style.transform = 'rotate(' + (date.getHours() * 6) + 'deg)';
-    min.style.transform = 'rotate(' + (date.getMinutes() * 6) + 'deg)';
-    sec.style.transform = 'rotate(' + (date.getSeconds() * 6) + 'deg)';
-    // console.log(date.getHours());
-    // console.log(date.getMinutes());
-    // console.log(date.getSeconds());
-
+    hr.style.transform = 'rotate(' + (((date.getHours() - 12) * 30) + 90 + 'deg');
+    min.style.transform = 'rotate(' + ((date.getMinutes() * 6) + 90 + 'deg');
+    sec.style.transform = 'rotate(' + ((date.getSeconds() * 6) + 90 + 'deg');
 }, 1000);
