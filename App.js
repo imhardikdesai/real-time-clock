895221c5f3efe46f2157c8e29b857b31f31bf419
hardik <hp676913@gmail.com>
2022-04-22 21:38:09 +0530

    Add js

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ setInterval(() => {
     let min = document.getElementById('min');
     let sec = document.getElementById('sec');
     let hr = document.getElementById('hr');
-    hr.style.transform = 'rotate(' + (date.getHours() + 6) + 'deg)';
-    min.style.transform = 'rotate(' + (date.getMinutes() + 6) + 'deg)';
-    sec.style.transform = 'rotate(' + (date.getSeconds() + 6) + 'deg)';
+    hr.style.transform = 'rotate(' + (date.getHours() * 6) + 'deg)';
+    min.style.transform = 'rotate(' + (date.getMinutes() * 6) + 'deg)';
+    sec.style.transform = 'rotate(' + (date.getSeconds() * 6) + 'deg)';
     // console.log(date.getHours());
     // console.log(date.getMinutes());
     // console.log(date.getSeconds());
